2f98cd7c5a5c0cf0919937a39f4c6cb4e19868a3
fvmachi <f.v.macc@hotmail.com>
2013-09-18 09:02:10 -0400

    Added getters and setters in Item object

--- futuretempglass/src/items/Item.java
+++ futuretempglass/src/items/Item.java
@@ -1,5 +1,6 @@
 package items;
 
+import java.util.Hashtable;
 import java.util.List;
 
 import enums.ProductionSteps;
@@ -11,5 +12,47 @@ public class Item{
 	private int quantity;
 
 	private List<ProductionSteps> productionSteps;
+
+	private Hashtable<String, String> attributes;
+
+	public String getItemName()
+	{
+		return itemName;
+	}
+
+	public void setItemName(String itemName)
+	{
+		this.itemName = itemName;
+	}
+
+	public int getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(int quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public List<ProductionSteps> getProductionSteps()
+	{
+		return productionSteps;
+	}
+
+	public void setProductionSteps(List<ProductionSteps> productionSteps)
+	{
+		this.productionSteps = productionSteps;
+	}
+
+	public Hashtable<String, String> getAttributes()
+	{
+		return attributes;
+	}
+
+	public void setAttributes(Hashtable<String, String> attributes)
+	{
+		this.attributes = attributes;
+	}
 
 }
