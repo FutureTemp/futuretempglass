d7ebf27566f1dbae6e4eb5cf2be1ef714acd8655
fvmachi <f.v.macc@hotmail.com>
2013-09-18 12:56:20 -0400

    Added WorkFlow Object

---
+++ futuretempglass/src/WorkFlow/WorkFlow.java
@@ -0,0 +1,14 @@
+package WorkFlow;
+
+import items.Item;
+
+import java.util.Hashtable;
+import java.util.List;
+
+import enums.ProductionSteps;
+
+public class WorkFlow{
+
+	private Hashtable<ProductionSteps, List<Item>> workFlow;
+	
+}
