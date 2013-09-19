e88cda55083359d190d9851ee7236bfc3998d35e
fvmachi <f.v.macc@hotmail.com>
2013-09-18 23:35:25 -0400

    Added code in Start to test xml functionality

--- futuretempglass/src/Start.java
+++ futuretempglass/src/Start.java
@@ -1,12 +1,60 @@
+import java.util.ArrayList;
+
+import xml.ItemAttributeXml;
+import xml.ItemXml;
+import xml.OrderXml;
+
 public class Start{
 
 	/**
 	 * @param args
+	 * @throws Exception
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		System.out.println("Hello World");
-		// test comments
+		OrderXml order = new OrderXml();
+		order.orderNumber = "12345";
+
+		ItemXml item = new ItemXml();
+		item.attributes = new ArrayList<ItemAttributeXml>();
+		ItemAttributeXml attribute = new ItemAttributeXml();
+
+		item.name = "glass";
+
+		attribute.name = "width";
+		attribute.value = "5";
+		item.attributes.add(attribute);
+
+		attribute = new ItemAttributeXml();
+		attribute.name = "height";
+		attribute.value = "10";
+		item.attributes.add(attribute);
+
+		order.items.add(item);
+
+		item.name = "crate";
+
+		attribute.name = "material";
+		attribute.value = "wood";
+
+		item.attributes.add(attribute);
+
+		order.items.add(item);
+
+		order.saveOrder();
+
+		order = OrderXml.loadOrder("12345");
+
+		System.out.println("Items: " + order.items.size());
+		for(ItemXml i: order.items)
+		{
+			System.out.println("Item name: " + i.name);
+			System.out.println("\tItem quantity: " + i.quantity);
+			for(ItemAttributeXml att: i.attributes)
+			{
+				System.out.println("\tItem " + att.name + ": " + att.value);
+			}
+		}
 	}
 
 }
