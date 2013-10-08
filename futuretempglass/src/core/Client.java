c7e57fd33e1ccda23d7b888a849d2a0e3ab340ea
fvmachi <f.v.macc@hotmail.com>
2013-10-07 22:19:27 -0400

    Made changes to Server and Client classes

--- futuretempglass/src/core/Client.java
+++ futuretempglass/src/core/Client.java
@@ -1,16 +1,19 @@
 package core;
 
-import items.Item;
-
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
 
 public class Client{
 
 	public static String serverIp = "192.168.1.14";
+
+	public static int TIMEOUT = 20000;
+
+	public static int OUT_PORT = 1110;
+
+	public static int IN_PORT = 1111;
 
 	public static void sendString(String string, InetAddress address, int port)
 	{
@@ -18,54 +21,16 @@ public class Client{
 		DatagramSocket ds = null;
 		try
 		{
-			ds = new DatagramSocket(1110);
+			ds = new DatagramSocket(OUT_PORT);
 			ds.send(new DatagramPacket(bytes, bytes.length, address, port));
 		}
 		catch(Exception e)
 		{
-
+			e.printStackTrace();
 		}
 		finally
 		{
 			if(ds != null)
-			{
-				ds.close();
-			}
-		}
-	}
-
-	public Client()
-	{
-		DatagramSocket ds = null;
-		try
-		{
-			ds = new DatagramSocket(1110);
-		}
-		catch(Exception e)
-		{
-
-		}
-		finally
-		{
-			if(ds != null)
-			{
-				ds.close();
-			}
-		}
-		while (true)
-		{
-			try
-			{
-				byte[] buffer = new byte[1024];
-				DatagramPacket p = new DatagramPacket(buffer, buffer.length);
-				ds.receive(p);
-				Client.execute(p);
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-			finally
 			{
 				ds.close();
 			}
@@ -77,7 +42,8 @@ public class Client{
 		DatagramSocket ds = null;
 		try
 		{
-			ds = new DatagramSocket(1110);
+			ds = new DatagramSocket(IN_PORT);
+			ds.setSoTimeout(TIMEOUT);
 			byte[] buffer = new byte[32768];
 			DatagramPacket p = new DatagramPacket(buffer, buffer.length);
 			ds.receive(p);
@@ -97,34 +63,11 @@ public class Client{
 		return null;
 	}
 
-	public static void execute(DatagramPacket packet)
-	{
-		String command = new String(packet.getData(), 0, packet.getLength());
-
-		if("get items".equalsIgnoreCase(command))
-		{
-			StringBuilder builder = new StringBuilder();
-			List<Item> items = Application.getItemLibrary().getItems();
-			for(Item item: items)
-			{
-				builder.append(item.getItemName());
-				builder.append('|');
-			}
-			sendString(builder.toString(), packet.getAddress(),
-					packet.getPort());
-		}
-	}
-
-	public static void main(String[] args)
-	{
-		new Client();
-	}
-
 	public static String sendMessageToServer(String string)
 	{
 		try
 		{
-			sendString(string, InetAddress.getByName(serverIp), 1110);
+			sendString(string, InetAddress.getByName(serverIp), OUT_PORT);
 			return receiveMessage();
 		}
 		catch(UnknownHostException e)
@@ -132,8 +75,6 @@ public class Client{
 			e.printStackTrace();
 		}
 
-		
-		
 		return null;
 	}
 }
--- futuretempglass/src/core/Server.java
+++ futuretempglass/src/core/Server.java
@@ -2,25 +2,52 @@ package core;
 
 import items.Item;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 import storage.server.ServerItemLibrary;
+import ui.views.Window;
 
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
 
-class Server{
+class Server extends Window implements MouseListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static int OUT_PORT = 1111;
+
+	private static int IN_PORT = 1110;
 
 	private static boolean stop = false;
-	
+
+	private static final int TIMEOUT = 10;
+
+	private JButton stopServerButton;
+
+	private JTextArea textArea;
+
 	public static void sendString(String string, InetAddress address, int port)
 	{
 		byte[] bytes = string.getBytes();
 		DatagramSocket ds = null;
 		try
 		{
-			ds = new DatagramSocket(1111);
+			ds = new DatagramSocket(OUT_PORT);
 			ds.send(new DatagramPacket(bytes, bytes.length, address, port));
 		}
 		catch(Exception e)
@@ -38,31 +65,39 @@ class Server{
 
 	public Server()
 	{
-		DatagramSocket ds = null;
-		try
-		{
-			ds = new DatagramSocket(1110);
-		}
-		catch(Exception e)
-		{
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		getContentPane().setLayout(
+				new FormLayout(new ColumnSpec[] {
+						FormFactory.RELATED_GAP_COLSPEC,
+						ColumnSpec.decode("default:grow"), }, new RowSpec[] {
+						FormFactory.RELATED_GAP_ROWSPEC,
+						RowSpec.decode("default:grow"),
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC, }));
 
-		}
-		finally
-		{
-			if(ds != null)
-			{
-				ds.close();
-			}
-		}
+		textArea = new JTextArea();
+		getContentPane().add(textArea, "2, 2, fill, fill");
+
+		stopServerButton = new JButton("Stop Server");
+		stopServerButton.addMouseListener(this);
+		getContentPane().add(stopServerButton, "2, 4");
+		pack();
+		setVisible(true);
+
+		DatagramSocket ds = null;
 		while (!stop)
 		{
 			try
 			{
-				ds = new DatagramSocket(1110);
+				ds = new DatagramSocket(IN_PORT);
+				ds.setSoTimeout(TIMEOUT);
 				byte[] buffer = new byte[1024];
 				DatagramPacket p = new DatagramPacket(buffer, buffer.length);
 				ds.receive(p);
-				Server.execute(p);
+				execute(p);
+			}
+			catch(SocketTimeoutException e)
+			{
 			}
 			catch(Exception e)
 			{
@@ -73,14 +108,15 @@ class Server{
 				ds.close();
 			}
 		}
+		dispose();
 	}
 
-	public static void execute(DatagramPacket packet)
+	public void execute(DatagramPacket packet)
 	{
 		String command = new String(packet.getData(), 0, packet.getLength());
 
-		System.out.println(command);
-		
+		textArea.insert(command + "\n", 0);
+
 		if("get item names".equalsIgnoreCase(command))
 		{
 			StringBuilder builder = new StringBuilder();
@@ -99,7 +135,43 @@ class Server{
 	public static void main(String[] args)
 	{
 		Application.itemLibrary = new ServerItemLibrary();
-		
 		new Server();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		if(e.getSource().equals(stopServerButton))
+		{
+			stop = true;
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+
 	}
 }
