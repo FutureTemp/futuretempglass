0499986d8e77445af4888905f04f974865dd8544
fvmachi <f.v.macc@hotmail.com>
2013-10-12 12:23:51 -0400

    Created a lot of the order creation UI interface

--- futuretempglass/src/ui/views/NewOrderWindow.java
+++ futuretempglass/src/ui/views/NewOrderWindow.java
@@ -2,21 +2,19 @@ package ui.views;
 
 import items.Item;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.ScrollPane;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.Enumeration;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
 
-import ui.components.InputQuestion;
+import orders.Order;
+import ui.components.ItemOrderComponent;
 
 import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.ColumnSpec;
@@ -25,126 +23,104 @@ import com.jgoodies.forms.layout.RowSpec;
 
 public class NewOrderWindow extends Window implements MouseListener{
 
-	ItemListWindow itemListWindow;
+	private ItemListWindow itemListWindow;
+	
+	JButton addItemButton;
 
-	JButton btnNewButton;
+	private Order order;
+
+	private JPanel itemsPanel;
+
+	public NewOrderWindow()
+	{
+		this(new Order());
+	}
 
 	/**
 	 * Create the frame.
 	 */
-	public NewOrderWindow()
+	public NewOrderWindow(Order order)
 	{
-		setBounds(100, 100, 450, 300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.order = order;
+		if(order.getItems() == null)
+		{
+			order.setItems(new ArrayList<Item>());
+		}
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 		getContentPane().setLayout(
 				new FormLayout(new ColumnSpec[] {
 						FormFactory.RELATED_GAP_COLSPEC,
-						FormFactory.DEFAULT_COLSPEC,
-						FormFactory.RELATED_GAP_COLSPEC,
-						ColumnSpec.decode("max(139dlu;default):grow"), },
-						new RowSpec[] { FormFactory.RELATED_GAP_ROWSPEC,
-								RowSpec.decode("max(164dlu;default):grow"), }));
-
-		btnNewButton = new JButton("New button");
-		btnNewButton.addMouseListener(this);
-		getContentPane().add(btnNewButton, "2, 2, default, bottom");
+						ColumnSpec.decode("default:grow"), }, new RowSpec[] {
+						FormFactory.RELATED_GAP_ROWSPEC,
+						RowSpec.decode("default:grow"),
+						FormFactory.RELATED_GAP_ROWSPEC,
+						RowSpec.decode("default:grow"), }));
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane
-				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		getContentPane().add(scrollPane, "4, 2, fill, fill");
+		scrollPane.getVerticalScrollBar().setUnitIncrement(16);
 
-		JPanel panel = new JPanel();
-		scrollPane.setViewportView(panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 0, 0 };
-		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
-		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-				0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
-		panel.setLayout(gbl_panel);
+		JPanel informationPane = new JPanel();
+		getContentPane().add(informationPane, "2, 2, fill, fill");
 
-		InputQuestion inputQuestion = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion = new GridBagConstraints();
-		gbc_inputQuestion.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion.gridx = 0;
-		gbc_inputQuestion.gridy = 0;
-		panel.add(inputQuestion, gbc_inputQuestion);
+		addItemButton = new JButton("Add Item");
+		addItemButton.addMouseListener(this);
+		informationPane.add(addItemButton);
+		getContentPane().add(scrollPane, "2, 4, fill, fill");
 
-		InputQuestion inputQuestion_1 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_1 = new GridBagConstraints();
-		gbc_inputQuestion_1.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_1.gridx = 0;
-		gbc_inputQuestion_1.gridy = 1;
-		panel.add(inputQuestion_1, gbc_inputQuestion_1);
+		itemsPanel = new JPanel();
+		scrollPane.setViewportView(itemsPanel);
+		/*scrollPane.setViewportView(itemsPanel);
+		itemsPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"), }, new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"), }));
 
-		InputQuestion inputQuestion_3 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_3 = new GridBagConstraints();
-		gbc_inputQuestion_3.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_3.gridx = 0;
-		gbc_inputQuestion_3.gridy = 2;
-		panel.add(inputQuestion_3, gbc_inputQuestion_3);
+		ItemOrderComponent itemOrderComponent_3 = new ItemOrderComponent();
+		itemsPanel.add(itemOrderComponent_3, "2, 2, fill, fill");
 
-		InputQuestion inputQuestion_2 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_2 = new GridBagConstraints();
-		gbc_inputQuestion_2.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_2.gridx = 0;
-		gbc_inputQuestion_2.gridy = 3;
-		panel.add(inputQuestion_2, gbc_inputQuestion_2);
+		ItemOrderComponent itemOrderComponent_2 = new ItemOrderComponent();
+		itemsPanel.add(itemOrderComponent_2, "2, 4, fill, fill");
 
-		InputQuestion inputQuestion_4 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_4 = new GridBagConstraints();
-		gbc_inputQuestion_4.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_4.gridx = 0;
-		gbc_inputQuestion_4.gridy = 4;
-		panel.add(inputQuestion_4, gbc_inputQuestion_4);
+		ItemOrderComponent itemOrderComponent_1 = new ItemOrderComponent();
+		itemsPanel.add(itemOrderComponent_1, "2, 6, fill, fill");
 
-		InputQuestion inputQuestion_5 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_5 = new GridBagConstraints();
-		gbc_inputQuestion_5.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_5.gridx = 0;
-		gbc_inputQuestion_5.gridy = 5;
-		panel.add(inputQuestion_5, gbc_inputQuestion_5);
-
-		InputQuestion inputQuestion_9 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_9 = new GridBagConstraints();
-		gbc_inputQuestion_9.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_9.gridx = 0;
-		gbc_inputQuestion_9.gridy = 6;
-		panel.add(inputQuestion_9, gbc_inputQuestion_9);
-
-		InputQuestion inputQuestion_6 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_6 = new GridBagConstraints();
-		gbc_inputQuestion_6.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_6.gridx = 0;
-		gbc_inputQuestion_6.gridy = 7;
-		panel.add(inputQuestion_6, gbc_inputQuestion_6);
-
-		InputQuestion inputQuestion_8 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_8 = new GridBagConstraints();
-		gbc_inputQuestion_8.insets = new Insets(0, 0, 5, 0);
-		gbc_inputQuestion_8.gridx = 0;
-		gbc_inputQuestion_8.gridy = 8;
-		panel.add(inputQuestion_8, gbc_inputQuestion_8);
-
-		InputQuestion inputQuestion_7 = new InputQuestion((String)null,
-				(String)null);
-		GridBagConstraints gbc_inputQuestion_7 = new GridBagConstraints();
-		gbc_inputQuestion_7.gridx = 0;
-		gbc_inputQuestion_7.gridy = 9;
-		panel.add(inputQuestion_7, gbc_inputQuestion_7);
-
-		pack();
+		ItemOrderComponent itemOrderComponent = new ItemOrderComponent();
+		itemsPanel.add(itemOrderComponent, "2, 8, fill, fill");*/
+		populateItemsInView();
+		// pack();
 		setVisible(true);
+	}
+
+	private void populateItemsInView()
+	{
+		RowSpec[] rowSpec = new RowSpec[order.getItems().size() * 2];
+		for(int i = 0; i < rowSpec.length; i++)
+		{
+			rowSpec[i++] = FormFactory.RELATED_GAP_ROWSPEC;
+			rowSpec[i] = RowSpec.decode("default:grow");
+		}
+
+		itemsPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"), }, rowSpec));
+
+		for(int i = 0; i < order.getItems().size(); i++)
+		{
+			ItemOrderComponent itemComponent = new ItemOrderComponent(order
+					.getItems().get(i));
+			itemsPanel.add(itemComponent, "2, " + (i + 1) * 2 + ", fill, fill");
+		}
+		itemsPanel.setVisible(true);
+		pack();
 	}
 
 	@Override
@@ -161,10 +137,13 @@ public class NewOrderWindow extends Window implements MouseListener{
 		else if(source instanceof EditItemWindow)
 		{
 			Item item = (Item)object;
+			order.getItems().add(item);
+			populateItemsInView();
 			System.out.println(item.getItemName());
-			for (String attributeName: item.getAttributeNames())
+			for(String attributeName: item.getAttributeNames())
 			{
-				System.out.println(attributeName + " : " + item.getAttribute(attributeName));
+				System.out.println(attributeName + " : "
+						+ item.getAttribute(attributeName));
 			}
 			System.out.println();
 		}
@@ -174,7 +153,7 @@ public class NewOrderWindow extends Window implements MouseListener{
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
-		if(e.getSource().equals(btnNewButton))
+		if(e.getSource().equals(addItemButton))
 		{
 			itemListWindow = new ItemListWindow(this);
 		}
